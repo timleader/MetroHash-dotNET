e1bf0bbe7cf266eaca1d54d917634ab73ebd0069
Tim Leader <tim@nerfgames.co.uk>
2015-07-13 19:57:30 +0100

    URL Conflict Tests

--- test/MetroHashTest.cs
+++ test/MetroHashTest.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Text;
+using System.Collections.Generic;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -9,7 +10,7 @@ using MetroHash;
 namespace MetroHash.Test
 {
     [TestClass]
-    public class MetroHashTest
+    public partial class MetroHashTest
     {
         //---------------------------------------------------------------------------//
         private static readonly string TEST_KEY_63 = 
@@ -19,12 +20,6 @@ namespace MetroHash.Test
         private static readonly byte[] TEST_HASH64_1 = 
         {
             0x65, 0x8F, 0x04, 0x4F, 0x5C, 0x73, 0x0E, 0x40
-        };
-
-        //---------------------------------------------------------------------------//
-        private static readonly byte[] TEST_HASH64_2 = 
-        {
-            0x07, 0x3C, 0xAA, 0xB9, 0x60, 0x62, 0x32, 0x11
         };
 
         //---------------------------------------------------------------------------//
@@ -43,6 +38,12 @@ namespace MetroHash.Test
         }
 
         //---------------------------------------------------------------------------//
+        private static readonly byte[] TEST_HASH64_2 = 
+        {
+            0x07, 0x3C, 0xAA, 0xB9, 0x60, 0x62, 0x32, 0x11
+        };
+
+        //---------------------------------------------------------------------------//
         [TestMethod]
         public void Hash64_2_SanityTest()
         {
@@ -59,9 +60,24 @@ namespace MetroHash.Test
 
         //---------------------------------------------------------------------------//
         [TestMethod]
-        public void Hash64_1_ConflictTest()
+        public void Hash64_1_URLConflictTest()
         {
-            Assert.AreEqual(1, 1);
+            HashSet<ulong> lHashCollection = new HashSet<ulong>();
+
+            byte[] lHashData = null;
+            byte[] lInput = null;
+            for (int lURLIndex = 0; lURLIndex < TEST_URL_LIST.Length; ++lURLIndex)
+            {
+                lInput = Encoding.UTF8.GetBytes(TEST_URL_LIST[lURLIndex]);
+
+                MetroHash.Hash64_2(lInput, 0, (uint)lInput.Length, 0, out lHashData);
+
+                ulong lHash = BitConverter.ToUInt64(lHashData, 0);
+
+                Assert.IsFalse(lHashCollection.Contains(lHash));
+
+                lHashCollection.Add(lHash);
+            }
         }
     }
 }
---
+++ test/MetroHashTestURL.cs
@@ -0,0 +1,19 @@
+
+using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MetroHash.Test
+{
+    [TestClass]
+    public partial class MetroHashTest
+    {
+
+        //---------------------------------------------------------------------------//
+        private static readonly string[] TEST_URL_LIST = 
+        {
+            "http://www.google.co.uk",
+        };
+
+    }
+}
