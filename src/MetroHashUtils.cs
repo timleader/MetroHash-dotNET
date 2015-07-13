b830b891bfa2de2d16e03179d29ed3168e89d27a
Tim Leader <tim@nerfgames.co.uk>
2015-07-13 18:05:18 +0100

    Added initial unit test project

--- src/MetroHashUtils.cs
+++ src/MetroHashUtils.cs
@@ -6,6 +6,7 @@ namespace MetroHash
 
     public partial class MetroHash
     {
+        //---------------------------------------------------------------------------//
         /* rotate right idiom recognized by compiler*/
         private static ulong RotateRight(ulong lV, uint lK)
         {
@@ -13,22 +14,26 @@ namespace MetroHash
             return (lV >> lSignedK) | (lV << (64 - lSignedK));
         }
 
+        //---------------------------------------------------------------------------//
         // unaligned reads, fast and safe on Nehalem and later microarchitectures
         private static ulong Read_u64(byte[] lData, uint lOffset)
         {
             return BitConverter.ToUInt64(lData, (int)lOffset);
         }
 
+        //---------------------------------------------------------------------------//
         private static ulong Read_u32(byte[] lData, uint lOffset)
         {
             return BitConverter.ToUInt32(lData, (int)lOffset);
         }
 
+        //---------------------------------------------------------------------------//
         private static ulong Read_u16(byte[] lData, uint lOffset)
         {
             return BitConverter.ToUInt16(lData, (int)lOffset);
         }
 
+        //---------------------------------------------------------------------------//
         private static ulong Read_u8(byte[] lData, uint lOffset)
         {
             return lData[lOffset];
---
+++ test/MetroHashTest.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MetroHash;
+
+namespace MetroHash.Test
+{
+    [TestClass]
+    public class MetroHashTest
+    {
+        [TestMethod, Timeout(200)]
+        public void MetroHash64SanityTest()
+        {
+            //MetroHash.Hash64_1(null, 0, 0, 0, out null);
+
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod, Timeout(200)]
+        public void MetroHash64ConflictTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
